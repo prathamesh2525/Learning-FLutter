92c02c8351e9e27cfd7ae1b4fc870b92d78435b6
Prathamesh madniwale <61145586+prathamesh2525@users.noreply.github.com>
2021-04-27 20:11:12 +0530

    error corrected

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  final int days = 15;
+  final int days = 30;
   final String name = "Prathamesh";
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return HomePage();
+    return MaterialApp(
+      home: HomePage(),
+    );
   }
 }
